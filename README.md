0638f10f0ca8c0fdb13116721c7f2c7132902d0e
caleb <16196262+spookyuser@users.noreply.github.com>
2024-03-21 09:59:43 +0200

    Make cli work for non git repos and make file types configurable

--- README.md
+++ README.md
@@ -2,11 +2,10 @@
 
 A cli written by claude for uploading repos to claude. Thx claude.
 
-`bun cli.ts <repoPath> <outputFilePath>`
-
+`bun cli.ts <repoPath> <outputFilePath>  ts tsx`
 
 # Examples
 
-`bun run cli.ts . output.txt`
+`bun run cli.ts . output.txt  ts tsx`
 
 https://bun.sh/
--- cli.ts
+++ cli.ts
@@ -1,117 +1,159 @@
-import { execSync } from "child_process"
-import fs from "fs"
-import path from "path"
+import { execSync } from "child_process";
+import fs from "fs";
+import path from "path";
 
 // Get the command-line arguments
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 
-// Check if the required arguments are provided
-if (args.length !== 2) {
-  console.error("Usage: bun script.js <repoPath> <outputFilePath>")
-  process.exit(1)
+// Check if the minimum required arguments are provided
+if (args.length < 2) {
+  console.error(
+    "Usage: node script.js <repoPath> <outputFilePath> [extensions...]"
+  );
+  process.exit(1);
 }
 
-const repoPath = args[0]
-const outputFilePath = args[1]
+const repoPath = args[0];
+const outputFilePath = args[1];
+
+const extensions = args.slice(2).map((ext) => `.${ext}`); // Ensure each extension starts with a dot
+extensions.push(".ts");
+extensions.push(".tsx");
 
 // Customize the separators
-const startSeparator = "===== BEGIN "
-const endSeparator = " ====="
+const startSeparator = "===== BEGIN ";
+const endSeparator = " =====";
 
-// Custom exclusions
-const customExclusions = [
-  "locales/",
-  "*.ts-snapshots"
-
-  // Add more custom exclusions as needed
-]
+// Standard exclusions
+const standardExclusions = [
+  "node_modules/",
+  ".git/",
+  ".DS_Store",
+  "dist/",
+  "build/",
+  "*.log",
+  // Add more exclusions as needed
+];
 
 // Custom includes
 const customIncludes = [
   "package.json",
   "../../README.md",
-  "README.md"
+  "README.md",
   // Add more custom includes as needed
-]
-function isExcluded(filePath) {
-  return customExclusions.some((exclusion) => {
-    const normalizedExclusion = path.normalize(exclusion)
-    const normalizedFilePath = path.normalize(filePath)
-    return normalizedFilePath.startsWith(normalizedExclusion)
-  })
-}
+];
 
-function isIncluded(filePath) {
-  return customIncludes.some((inclusion) => {
-    const normalizedInclusion = path.normalize(inclusion)
-    const normalizedFilePath = path.normalize(filePath)
-    return normalizedFilePath.startsWith(normalizedInclusion)
-  })
-}
-
-function getTrackedFiles() {
-  const output = execSync("git ls-files", { cwd: repoPath, encoding: "utf-8" })
-  return output.split("\n").filter((file) => file.trim() !== "")
-}
-
-function generateFileTree(files) {
-  const fileTree = {}
-
-  files.forEach((file) => {
-    if (!isExcluded(file) || isIncluded(file)) {
-      const parts = file.split(path.sep)
-      let currentLevel = fileTree
-
-      parts.forEach((part, index) => {
-        if (index === parts.length - 1) {
-          currentLevel[part] = true // Use `true` instead of `null` for leaf nodes
-        } else {
-          currentLevel[part] = currentLevel[part] || {}
-          currentLevel = currentLevel[part]
-        }
-      })
-    }
-  })
-
-  return fileTree
-}
-
-function printFileTree(fileTree, indent = "", isLast = true) {
-  const keys = Object.keys(fileTree)
-  const lastIndex = keys.length - 1
-
-  keys.forEach((key, index) => {
-    const isLastItem = index === lastIndex
-    const treeItem = isLastItem ? "└─ " : "├─ "
-    const subIndent = isLast ? "   " : "│  "
-
-    if (fileTree[key] === true) {
-      // Print leaf nodes (files)
-      fs.appendFileSync(outputFilePath, `${indent}${treeItem}${key}\n`)
-    } else {
-      // Print directories
-      fs.appendFileSync(outputFilePath, `${indent}${treeItem}${key}\n`)
-      printFileTree(fileTree[key], indent + subIndent, isLastItem)
-    }
-  })
-}
-
-function processFile(filePath) {
-  if (
-    (!isExcluded(filePath) || isIncluded(filePath)) &&
-    (filePath.endsWith(".ts") || filePath.endsWith(".tsx"))
-  ) {
-    const fileContent = fs.readFileSync(path.join(repoPath, filePath), "utf-8")
-    const separator = `${startSeparator}${filePath}${endSeparator}\n${fileContent}\n\n`
-    fs.appendFileSync(outputFilePath, separator)
+function isGitRepo(repoPath) {
+  try {
+    fs.accessSync(path.join(repoPath, ".git"), fs.constants.F_OK);
+    return true;
+  } catch {
+    return false;
   }
 }
 
-const trackedFiles = getTrackedFiles()
-const fileTree = generateFileTree(trackedFiles)
+function isExcluded(filePath) {
+  return standardExclusions.some((exclusion) => {
+    const normalizedExclusion = path.normalize(exclusion).replace(/\\/g, "/");
+    const normalizedFilePath = path.normalize(filePath).replace(/\\/g, "/");
+    if (normalizedExclusion.endsWith("/")) {
+      return normalizedFilePath.startsWith(normalizedExclusion);
+    }
+    return normalizedFilePath.endsWith(normalizedExclusion);
+  });
+}
 
-fs.writeFileSync(outputFilePath, "File Tree:\n")
-printFileTree(fileTree)
-fs.appendFileSync(outputFilePath, "\n")
+function isIncluded(filePath) {
+  return (
+    customIncludes.some((inclusion) => {
+      const normalizedInclusion = path.normalize(inclusion);
+      const normalizedFilePath = path.normalize(filePath);
+      return normalizedFilePath.includes(normalizedInclusion);
+    }) || extensions.some((ext) => filePath.endsWith(ext))
+  );
+}
 
-trackedFiles.forEach(processFile)
+function listFiles(dir, fileList = []) {
+  const files = fs.readdirSync(dir);
+  files.forEach((file) => {
+    const filePath = path.join(dir, file);
+    if (fs.statSync(filePath).isDirectory()) {
+      listFiles(filePath, fileList);
+    } else {
+      fileList.push(filePath.replace(`${repoPath}/`, ""));
+    }
+  });
+  return fileList;
+}
+
+function getTrackedFiles() {
+  const output = execSync("git ls-files", { cwd: repoPath, encoding: "utf-8" });
+  return output.split("\n").filter((file) => file.trim() !== "");
+}
+
+function getAllFiles() {
+  if (isGitRepo(repoPath)) {
+    return getTrackedFiles();
+  } else {
+    return listFiles(repoPath).filter(
+      (file) => !isExcluded(file) || isIncluded(file)
+    );
+  }
+}
+
+const allFiles = getAllFiles();
+const fileTree = generateFileTree(allFiles);
+
+fs.writeFileSync(outputFilePath, "File Tree:\n");
+printFileTree(fileTree);
+fs.appendFileSync(outputFilePath, "\n\n");
+
+// Adjusted to consider configurable extensions
+allFiles.forEach((filePath) => {
+  if (isIncluded(filePath)) {
+    const fileContent = fs.readFileSync(path.join(repoPath, filePath), "utf-8");
+    const separator = `${startSeparator}${filePath}${endSeparator}\n${fileContent}\n\n`;
+    fs.appendFileSync(outputFilePath, separator);
+  }
+});
+function printFileTree(fileTree, indent = "", isLast = true) {
+  const keys = Object.keys(fileTree);
+  const lastIndex = keys.length - 1;
+
+  keys.forEach((key, index) => {
+    const isLastItem = index === lastIndex;
+    const treeItem = isLastItem ? "└─ " : "├─ ";
+    const subIndent = isLast ? "   " : "│  ";
+
+    if (fileTree[key] === true) {
+      // Print leaf nodes (files)
+      fs.appendFileSync(outputFilePath, `${indent}${treeItem}${key}\n`);
+    } else {
+      // Print directories
+      fs.appendFileSync(outputFilePath, `${indent}${treeItem}${key}\n`);
+      printFileTree(fileTree[key], indent + subIndent, isLastItem);
+    }
+  });
+}
+
+function generateFileTree(files) {
+  const fileTree = {};
+
+  files.forEach((file) => {
+    if (!isExcluded(file) || isIncluded(file)) {
+      const parts = file.split(path.sep);
+      let currentLevel = fileTree;
+
+      parts.forEach((part, index) => {
+        if (index === parts.length - 1) {
+          currentLevel[part] = true; // Use `true` instead of `null` for leaf nodes
+        } else {
+          currentLevel[part] = currentLevel[part] || {};
+          currentLevel = currentLevel[part];
+        }
+      });
+    }
+  });
+
+  return fileTree;
+}
